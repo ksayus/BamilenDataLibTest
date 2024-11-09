c52e656c1860c80096bd8dbebc1d8793f85d5cba
ksayus <2481374335@qq.com>
2024-11-10 00:58:38 +0800

    添加项目文件。

---
+++ Bamilen数据库模拟/BamilenDataLib.cpp
@@ -0,0 +1,438 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <direct.h>
+#include <io.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <filesystem>
+
+#define MaxAccountQuality 1001
+#define MaxFolderAccountQuality 100
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+void ShowMenu()
+{
+	cout << "1.添加用户" << endl;
+	cout << "2.删除用户" << endl;
+	cout << "3.显示用户" << endl;
+	cout << "4.查找用户" << endl;
+	cout << "5.修改用户数据" << endl;
+	cout << "6.格式化数据库" << endl;
+	cout << "0.退出数据库" << endl;
+}
+
+struct User
+{
+	int AccountID;
+	string AccountName;
+	string AccountPassword;
+	string EmailAddress;
+	string RegisterTime;
+	bool LoginAccountSafetyCheck;
+};
+
+struct DataLib
+{
+	struct User Account[MaxAccountQuality];
+	int UserID;
+};
+
+tm* GetTime()
+{
+	time_t nowtime;
+	time(&nowtime); //获取1970年1月1日0点0分0秒到现在经过的秒数
+	tm* NowTime = localtime(&nowtime); //将秒数转换为本地时间,年从1900算起,需要+1900,月为0-11,所以要+1
+	return NowTime;
+}
+
+void CheckDataLibFolder()
+{
+	string DataLibFolderPosition = "./DataLib";
+	if (_access(DataLibFolderPosition.c_str(), 0) == -1)
+	{
+		cout << "数据库不存在，创建中..." << endl;
+		int datalibfolderposition = _mkdir(DataLibFolderPosition.c_str());
+		cout << "创建完成！" << endl;
+	}
+	else {
+		cout << "文件夹存在" << endl;
+	}
+	system("cls");
+}
+
+void CheckAccountInformationFile()
+{
+	int FolderQuality = MaxAccountQuality / MaxFolderAccountQuality;
+	int TheRestQuality = MaxAccountQuality % MaxFolderAccountQuality;
+	int BeginQuality = 0;
+	int EndQuality = 0;
+	int LastEndQuality = 0;
+
+	for (int Folder = 0; Folder < FolderQuality; Folder++)
+	{
+		BeginQuality = (Folder*MaxFolderAccountQuality) + 1;
+		EndQuality = BeginQuality + (MaxFolderAccountQuality - 1);
+		string FolderPosition = "./DataLib/" + to_string(BeginQuality) + "-" + to_string(EndQuality);
+		if (_access(FolderPosition.c_str(), 0) == -1)
+		{
+			cout << "文件夹不存在，创建中..." << endl;
+			int folderposition = _mkdir(FolderPosition.c_str());
+			cout << "创建完成！" << endl;
+		}
+		else {
+			cout << "文件夹存在" << endl;
+		}
+		if (Folder == FolderQuality)
+		{
+			LastEndQuality = EndQuality + TheRestQuality;
+			string FolderPosition = "./DataLib/" + to_string(BeginQuality) + "-" + to_string(LastEndQuality);
+			if (_access(FolderPosition.c_str(), 0) == -1)
+			{
+				cout << "文件夹不存在，创建中..." << endl;
+				int folderposition = _mkdir(FolderPosition.c_str());
+				cout << "创建完成！" << endl;
+			}
+			else {
+				cout << "文件夹存在" << endl;
+			}
+		}
+	}
+	system("cls");
+}
+
+int ReadDataLib(DataLib *DL)
+{
+	int FolderQuality = MaxAccountQuality / MaxFolderAccountQuality;
+	int TheRestQuality = MaxAccountQuality % MaxFolderAccountQuality;
+	int BeginQuality = 0;
+	int EndQuality = 0;
+	int LastEndQuality = 0;
+
+	int AccountQuality = 1;
+
+	for (int Folder = 0; Folder < FolderQuality; Folder++)
+	{
+		BeginQuality = (Folder * MaxFolderAccountQuality) + 1;
+		EndQuality = BeginQuality + (MaxFolderAccountQuality - 1);
+		if (Folder != FolderQuality)
+		{
+			string CD = "cd ./DataLib/" + to_string(BeginQuality) + "-" + to_string(EndQuality);
+			for (int Check = BeginQuality; Check <= EndQuality; Check++)
+			{
+				ifstream ReadAccountJson("./DataLib/" + to_string(BeginQuality) + "-" + to_string(EndQuality) + "/" + to_string(Check) + ".json");
+				if (!ReadAccountJson.is_open())
+				{
+					cout << "没有该用户。" << endl;
+				}
+				else {
+					cout << "读取文件中..." << endl;
+					// 读取JSON文件到字符串
+					std::string json_str((std::istreambuf_iterator<char>(ReadAccountJson)), std::istreambuf_iterator<char>());
+					// 解析JSON字符串
+					nlohmann::json j = nlohmann::json::parse(json_str, nullptr, false);
+					
+					DL->Account[Check].AccountID = j["ID"];
+					DL->Account[Check].AccountName = j["Name"];
+					DL->Account[Check].EmailAddress = j["EmailAddress"];
+					DL->Account[Check].AccountPassword = j["Password"];
+					DL->Account[Check].RegisterTime = j["Register"];
+					DL->Account[Check].LoginAccountSafetyCheck = j["SafetyCheck"];
+					AccountQuality++;
+					cout << "读取完成！" << endl;
+				}
+			}
+		}
+		else {
+			LastEndQuality = EndQuality + TheRestQuality;
+			string CD = "cd ./DataLib/" + to_string(BeginQuality) + "-" + to_string(LastEndQuality);
+			for (int Check = BeginQuality; Check <= LastEndQuality; Check++)
+			{
+				ifstream ReadAccountJson("./DataLib/" + to_string(BeginQuality) + "-" + to_string(EndQuality) + "/" + to_string(Check) + ".json");
+				if (!ReadAccountJson.is_open())
+				{
+					cout << "没有该用户。" << endl;
+				}
+				else {
+					cout << "读取文件中..." << endl;
+					// 读取JSON文件到字符串
+					std::string json_str((std::istreambuf_iterator<char>(ReadAccountJson)), std::istreambuf_iterator<char>());
+					// 解析JSON字符串
+					nlohmann::json j = nlohmann::json::parse(json_str, nullptr, false);
+
+					DL->Account[Check].AccountID = j["ID"];
+					DL->Account[Check].AccountName = j["Name"];
+					DL->Account[Check].EmailAddress = j["EmailAddress"];
+					DL->Account[Check].AccountPassword = j["Password"];
+					DL->Account[Check].RegisterTime = j["Register"];
+					DL->Account[Check].LoginAccountSafetyCheck = j["SafetyCheck"];
+					AccountQuality++;
+					cout << "读取完成！" << endl;
+				}
+			}
+		}
+	}
+	system("cls");
+	return AccountQuality;
+}
+
+void AddAccount(DataLib * DL)
+{
+	system("cls");
+	if (DL->UserID == 0)
+	{
+		DL->UserID++;
+	}
+	if (DL->UserID == MaxAccountQuality)
+	{
+		cout << "用户数量已达上限，无法添加！" << endl;
+		return;
+	}
+
+	string Name;
+	string Password, Password_Again;
+	string Email;
+
+	cout << "请输入用户名：" << endl;
+	cin >> Name;
+	int Check = 1;
+	while (Check != MaxAccountQuality)
+	{
+		if (DL->Account[Check].AccountID == 0)
+		{
+			break;
+		}
+		if (Name == DL->Account[Check].AccountName)
+		{
+			cout << "账号邮箱重复，请重新输入" << endl;
+			cin >> Name;
+			Check = 1;
+		}
+		else {
+			Check++;
+		}
+	}
+
+	cout << "请输入密码：" << endl;
+	cin >> Password;
+	while (true)
+	{
+		cout << "再次输入密码以确认：" << endl;
+		cin >> Password_Again;
+		if (Password_Again == Password)
+		{
+			break;
+		}
+		cout << "两次输入的不一致，请重新输入：" << endl;
+	}
+
+	cout << "请输入邮箱：" << endl;
+	cin >> Email;
+	Check = 1;
+	while (Check != MaxAccountQuality)
+	{
+		if (DL->Account[Check].AccountID == 0)
+		{
+			break;
+		}
+		if (Email == DL->Account[Check].EmailAddress)
+		{
+			cout << "账号邮箱重复，请重新输入" << endl;
+			cin >> Email;
+			Check = 1;
+		}
+		else {
+			Check++;
+		}
+	}
+
+	string RegisterTime;
+	tm* RTime = GetTime();
+	RegisterTime = to_string(RTime->tm_year + 1900) + "-" + to_string(RTime->tm_mon + 1) + "-" + to_string(RTime->tm_mday) + "-" + to_string(RTime->tm_hour) + ":" + to_string(RTime->tm_min) + ":" + to_string(RTime->tm_sec);
+
+	DL->Account[DL->UserID].AccountName = Name;
+	DL->Account[DL->UserID].AccountPassword = Password_Again;
+	DL->Account[DL->UserID].EmailAddress = Email;
+	DL->Account[DL->UserID].AccountID = DL->UserID;
+	DL->Account[DL->UserID].LoginAccountSafetyCheck = true;
+	DL->Account[DL->UserID].RegisterTime = RegisterTime;
+
+	nlohmann::json j;
+	j["ID"] = DL->Account[DL->UserID].AccountID;
+	j["Name"] = DL->Account[DL->UserID].AccountName;
+	j["EmailAddress"] = DL->Account[DL->UserID].EmailAddress;
+	j["Password"] = DL->Account[DL->UserID].AccountPassword;
+	j["Register"] = DL->Account[DL->UserID].RegisterTime;
+	j["SafetyCheck"] = DL->Account[DL->UserID].LoginAccountSafetyCheck;
+	int BeginFolderPosition = DL->UserID / MaxFolderAccountQuality;
+	int EndFolderPosition = DL->UserID % MaxFolderAccountQuality;
+	int BeginFilePosition = (BeginFolderPosition * MaxFolderAccountQuality) + 1;
+	int EndFilePosition = BeginFilePosition + (MaxFolderAccountQuality - 1);
+
+	string FolderPosition = "./DataLib/" + to_string(BeginFilePosition) + "-" + to_string(EndFilePosition) + "/";
+	
+	string JsonFileName = FolderPosition + to_string(DL->UserID) + ".json";
+	ofstream UserJsonFile(JsonFileName);
+	UserJsonFile << j.dump(4);
+	if (UserJsonFile.good())
+	{
+		cout << "用户json文件创建成功！" << endl;
+	}
+	else {
+		cout << "用户json文件创建失败！" << endl;
+	}
+
+
+	cout << "添加完成！" << endl;
+	system("pause");
+	system("cls");
+}
+
+void DeleteAccount(DataLib *DL)
+{
+	int SelectAccountID = 0;
+	string Password;
+	cout << "输入你要删除的用户的ID：" << endl;
+	cin >> SelectAccountID;
+	while (true)
+	{
+		if (DL->Account[SelectAccountID].AccountID == 0)
+		{
+			cout << "用户不存在，请重新输入!" << endl;
+			cout << "输入你要删除的用户的ID：" << endl;
+			cin >> SelectAccountID;
+		}
+		else {
+			break;
+		}
+	}	
+
+	while (true)
+	{
+		cout << "输入密码以确认：" << endl;
+		cin >> Password;
+		if (Password == DL->Account[SelectAccountID].AccountPassword)
+		{
+			break;
+		}
+		cout << "密码错误，请重新输入！" << endl;
+	}
+
+	bool Ensure = true;
+
+	do
+	{
+		cout << "确认删除？（0同意，大于1不同意）" << endl;
+		cin >> Ensure;
+		if (Ensure == false)
+		{
+			break;
+		}
+		else {
+			system("cls");
+			return;
+		}
+	} while (true);
+
+	cout << "删除中..." << endl;
+	DL->Account[SelectAccountID].AccountID = 0;
+
+	int BeginFolderPosition = DL->UserID / MaxFolderAccountQuality;
+	int EndFolderPosition = DL->UserID % MaxFolderAccountQuality;
+	int BeginFilePosition = (BeginFolderPosition * MaxFolderAccountQuality) + 1;
+	int EndFilePosition = BeginFilePosition + (MaxFolderAccountQuality - 1);
+
+	string FolderPosition = "./DataLib/" + to_string(BeginFilePosition) + "-" + to_string(EndFilePosition) + "/" + to_string(SelectAccountID) + ".json";
+
+	// 删除文件
+	try {
+		// 检查文件是否存在
+		if (fs::exists(FolderPosition)) {
+			// 删除文件
+			fs::remove(FolderPosition);
+			std::cout << "文件已删除" << std::endl;
+			cout << "删除完成！" << endl;
+		}
+		else {
+			std::cout << "文件不存在" << std::endl;
+		}
+	}
+	catch (const fs::filesystem_error& e) {
+		std::cerr << "错误: " << e.what() << std::endl;
+	}
+	system("pause");
+	system("cls");
+}
+
+void ExhibitAccount(DataLib *DL)
+{
+	system("cls");
+	int AccountQuality = 0;
+	for (int Check = 1; Check < MaxAccountQuality; Check++)
+	{
+		if (AccountQuality == DL->UserID - 1)
+		{
+			system("pause");
+			system("cls");
+			return;
+		}
+		if (DL->Account[Check].AccountID == 0)
+		{
+			continue;
+		}
+		else {
+			cout << to_string(Check) + "." + DL->Account[Check].AccountName << endl;
+			AccountQuality++;
+		}
+	}
+}
+
+
+int main()
+{
+	CheckDataLibFolder();
+	CheckAccountInformationFile();
+
+	DataLib DL;
+	DL.UserID = 1;
+
+	int Quality = ReadDataLib(&DL);
+	DL.UserID = Quality;
+
+
+
+	int SelectFunction = 0;
+	while (true)
+	{
+		int Quality = ReadDataLib(&DL);
+		DL.UserID = Quality;
+		ShowMenu();
+		cin >> SelectFunction;
+		switch (SelectFunction)
+		{
+		case 1:AddAccount(&DL);
+			break;
+		case 2:DeleteAccount(&DL);
+			break;
+		case 3:ExhibitAccount(&DL);
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 0:cout << "欢迎再次使用！" << endl;
+			system("pause");
+			return 0;
+		default:
+			break;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
